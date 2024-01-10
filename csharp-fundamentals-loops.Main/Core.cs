09dc54462ac2ad9338e496852e59b0051a2447f9
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-01-10 09:41:45 +0100

    Implement unimplemented methods

--- csharp-fundamentals-loops.Main/Core.cs
+++ csharp-fundamentals-loops.Main/Core.cs
@@ -20,32 +20,55 @@ namespace csharp_fundamentals_loops.Main
         public void stepOne()
         {
             // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array                        
-            throw new NotImplementedException();            
+            for (int i = 0; i <= 3; i++)
+            {
+                numsZeroToThree[i] = i;
+            }      
         }
 
         public void stepTwo()
         {
             // TODO: 2. Write a for loop that adds the numbers 5 to 10 to the numsFiveToTen array
-            throw new NotImplementedException();
+            for (int i = 5, j = 0; i <= 10; i++, j++)
+            {
+                numsFiveToTen[j] = i;
+            }
         }
 
         public void stepThree()
         {
             // TODO: 3. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
-            throw new NotImplementedException();
+            for(int i = 3, j = 0; j <= 3; i--, j++)
+            {
+                countdown[j] = i;
+            }
 
         }
 
         public bool stepFour(int num)
         {
             // TODO: 6. Write a for loop that checks if num is in the favouriteNumbers array            
-            throw new NotImplementedException();
+            for (int i = 0; i < favouriteNumbers.Length; i++)
+            {
+                if (num == favouriteNumbers[i])
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool stepFive(string hobby)
         {
             // TODO 5. Write a for loop that checks if the hobby String is in the myHobbies array
-            throw new NotImplementedException();
+            foreach (string hobbyString in myHobbies)
+            {
+                if (hobbyString == hobby)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 
