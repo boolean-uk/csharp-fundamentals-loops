71008775e3a1774aee3d48b438601965b0fc763a
André Sturesson <git@asturesson.se>
2024-01-10 10:45:53 +0100

    Done

--- csharp-fundamentals-loops.Main/Core.cs
+++ csharp-fundamentals-loops.Main/Core.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,32 +21,55 @@ namespace csharp_fundamentals_loops.Main
         public void stepOne()
         {
             // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array                        
-            throw new NotImplementedException();            
+            for (int i = 0; i < numsZeroToThree.Length; i++)
+            {
+                numsZeroToThree[i] = i;
+            }
         }
 
         public void stepTwo()
         {
             // TODO: 2. Write a for loop that adds the numbers 5 to 10 to the numsFiveToTen array
-            throw new NotImplementedException();
+            int c = 5;
+            for (int i = 0; i < numsFiveToTen.Length; i++)
+            {
+                numsFiveToTen[i] = c;
+                c++;
+            }
         }
 
         public void stepThree()
         {
             // TODO: 3. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
-            throw new NotImplementedException();
+            int c = 0;
+            for (int i = countdown.Length-1; i >= 0; i--)
+            {
+                countdown[c] = i;
+                c++;
+            }
 
         }
 
         public bool stepFour(int num)
         {
             // TODO: 6. Write a for loop that checks if num is in the favouriteNumbers array            
-            throw new NotImplementedException();
+            foreach (int item in favouriteNumbers)
+            {
+                if(item == num)
+                    return true;
+            }
+            return false;
         }
 
         public bool stepFive(string hobby)
         {
             // TODO 5. Write a for loop that checks if the hobby String is in the myHobbies array
-            throw new NotImplementedException();
+            foreach (string item in myHobbies)
+            {
+                if (item == hobby)
+                    return true;
+            }
+            return false;
         }
     }
 
